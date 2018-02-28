c54ecdbaa896ff4652bec4ecb153b5b1201a99f3
zhangmengjing <1459149735@qq.com>
2018-02-28 11:22:35 +0800

    更新主页

--- myfirstvue/src/page/home/Home.vue
+++ myfirstvue/src/page/home/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="home">
     <h2>this is home </h2>
+    <div class="col1">
+      <div class="h_nav">
+        <h4>help</h4>
+        <ul>
+          <li><a href="products.html">trends</a></li>
+          <li><a href="products.html">sale</a></li>
+          <li><a href="products.html">style videos</a></li>
+          <li><a href="products.html">accessories</a></li>
+          <li><a href="products.html">kids</a></li>
+          <li><a href="products.html">style videos</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
